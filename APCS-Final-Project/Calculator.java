cceb8b68d491b84c3ea30842dc61f337e250a3ea
Akhil Aggarwal 2018 <aaggarw8@students.d125.org>
2016-05-13 08:44:13 -0500

    the updated version of calculating strings, doesn't fully work yet

---
+++ APCS-Final-Project/Calculator.java
@@ -0,0 +1,45 @@
+public class Calculator
+{
+    
+    public Calculator ()
+    {
+        
+    }
+        
+    public int add (int a, int b)
+    {
+        return a + b;
+    }
+    
+    public int subtract (int a, int b)
+    {
+        return a-b;
+    }
+    
+    public int multiply (int a, int b)
+    {
+        return a*b;
+    }
+    
+    public double divide (double a, double b)
+    {
+        return a/b;
+    }
+    
+    public int square(int a)
+    {
+        return a*a;
+    }
+    
+    public double squareRoot(double a)
+    {
+        return Math.sqrt(a);
+    }
+    //b is the base it's being converted to, a is the number you want to convert
+    public int convertToBase(int a, int b)
+    {
+        
+    }
+    
+    
+}
