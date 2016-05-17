c56b3bfe2035ef30ac33ca321cc3353258403231
aaggarw8 <aaggarw8@students.d125.org>
2016-05-17 09:21:04 -0500

    Add files via upload
    Finally got the multiplication to work for any length of string (for example: 7*7*9*3*4*5*6) would be calculated. However, the program still doesn't account for addition, subtraction, or division.

--- StringCalculator.java
+++ StringCalculator.java
@@ -6,37 +6,66 @@ public class StringCalculator extends Calculator
         Scanner sc = new Scanner(System.in);
         System.out.println("What is the problem you are trying to solve? ");
         String expression = sc.nextLine();
-        
+
         StringAnalyzer s1 = new StringAnalyzer(expression);
         ArrayList<String> values = new ArrayList<String>();
+
         for (int i =0; i<expression.length(); i++)
         {
             values.add(expression.substring(i, i+1));
         }
-        
-        while(s1.containsMultiply() == true || s1.containsDivide() == true)
+
+        while(s1.containsMultiply() || s1.containsDivide())
         {
             for (int i = 0; i < values.size(); i++)
             {
-                if (values.get(i) == "*")
+                if (values.get(i).equals("*"))
                 {
                     values.set(i, "" + (Integer.parseInt(values.get(i+1)) * Integer.parseInt(values.get(i-1))));
                     values.remove(i-1);
                     values.remove(i);
-                    System.out.println(values.get(i));
-                   
+                    i = 0;
+                    expression = "";
+                    for (String c: values)
+                    {
+                        expression+=c;
+                    }
+                    s1 = new StringAnalyzer(expression);
+
                 }
-                else if (values.get(i) == "/")
+                else if (values.get(i).equals("/"))
                 {
                     values.set(i, "" + (Integer.parseInt(values.get(i+1)) / Integer.parseInt(values.get(i-1))));
                     values.remove(i-1);
                     values.remove(i);
-                    i = i-2;
-                    
+                    i = 0;
+                    System.out.println(values.get(0));
+                    expression = "";
+                    for (String c: values)
+                    {
+                        expression+=c;
+                    }
+                    s1 = new StringAnalyzer(expression);
+
                 }
             }
             
+
+            /*else if (values.get(i) == "/")
+            {
+            values.set(i, "" + (Integer.parseInt(values.get(i+1)) / Integer.parseInt(values.get(i-1))));
+            values.remove(i-1);
+            values.remove(i);
+            i = i-2;
+
+            }
+            }
+             */
+            System.out.println(values.get(0));
+            
         }
+        //System.out.println(values.get(0));
+
         while (s1.containsAdd() == true || s1.containsSubtract() == true)
         {
             for (int i = 0; i < values.size(); i++)
@@ -54,37 +83,37 @@ public class StringCalculator extends Calculator
                     values.remove(i-1);
                     values.remove(i);
                     i = i-2;
-                    
+
                 }
             }
-            
+
         }
         //String value = "";
         /*for (int i = 0; i < values.size(); i++)
         {
-            value+=values.get(i);
+        value+=values.get(i);
         }
         /*ArrayList<String> values = new ArrayList<String>();
         for (int i =0; i<expression.length(); i++)
         {
-            values.add(expression.substring(i, i+1));
+        values.add(expression.substring(i, i+1));
         }
         for (int i =0; i < values.size(); i+=)
         {
-            if (values.get(i) == "*")
-            {
-                values.set(i, "" + values.get(i-1) * values.get(i+1);
-                i = i-3;
-            }
-            else if (values.get(i) == ("/")
-            {
-                
-            }
-            
+        if (values.get(i) == "*")
+        {
+        values.set(i, "" + values.get(i-1) * values.get(i+1);
+        i = i-3;
         }
-        */
-        
+        else if (values.get(i) == ("/")
+        {
+
+        }
+
+        }
+         */
+
         //System.out.println(value);
     }
-    
+
 }
