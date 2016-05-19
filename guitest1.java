652023306a1736cd2b8e469e62d4d6fbe8fad2e6
gvalent8 <gvalent8@students.d125.org>
2016-05-19 08:54:05 -0500

    Update guitest1.java
    Added random color button

--- guitest1.java
+++ guitest1.java
@@ -1,9 +1,10 @@
+import java.awt.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.cal;
+ 
 
 /**
  *
@@ -648,7 +649,11 @@ public class MyCal extends javax.swing.JFrame {
     }                                          
 
     private void colorActionPerformed(java.awt.event.ActionEvent evt) {                                      
-        // TODO add your handling code here:
+        int R = (int)(Math.random()*256);
+        int G = (int)(Math.random()*256);
+        int B= (int)(Math.random()*256);
+        Color RGB = new Color(R, G, B);
+        this.getContentPane().setBackground(RGB);
     }                                     
 
     /**
