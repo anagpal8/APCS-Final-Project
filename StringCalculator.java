c1af90d5f5042bf4ff843a842535cda1a9e623ac
aaggarw8 <aaggarw8@students.d125.org>
2016-05-19 08:26:54 -0500

    Add files via upload
    Does the order of operations correctly and returns the correct value. Now just need to make all values convert to a double rather than an int.

--- StringCalculator.java
+++ StringCalculator.java
@@ -35,11 +35,51 @@ public class StringCalculator extends Calculator
                 }
                 else if (values.get(i).equals("/"))
                 {
-                    values.set(i, "" + (Integer.parseInt(values.get(i+1)) / Integer.parseInt(values.get(i-1))));
+                    values.set(i, "" + (Integer.parseInt(values.get(i-1)) / Integer.parseInt(values.get(i+1))));
                     values.remove(i-1);
                     values.remove(i);
                     i = 0;
-                    System.out.println(values.get(0));
+
+                    expression = "";
+                    for (String c: values)
+                    {
+                        expression+=c;
+                    }
+                    s1 = new StringAnalyzer(expression);
+
+                }
+            }
+
+            
+            //System.out.println(values.get(0));
+
+        }
+        //System.out.println(values.get(0));
+
+        while (s1.containsAdd() == true || s1.containsSubtract() == true)
+        {
+            for (int i = 0; i < values.size(); i++)
+            {
+                if (values.get(i).equals("+"))
+                {
+                    values.set(i, "" + (Integer.parseInt(values.get(i+1)) + Integer.parseInt(values.get(i-1))));
+                    values.remove(i-1);
+                    values.remove(i);
+                    i = 0;
+                    expression = "";
+                    for (String c: values)
+                    {
+                        expression+=c;
+                    }
+                    s1 = new StringAnalyzer(expression);
+
+                }
+                else if (values.get(i).equals("-"))
+                {
+                    values.set(i, "" + (Integer.parseInt(values.get(i-1)) - Integer.parseInt(values.get(i+1))));
+                    values.remove(i-1);
+                    values.remove(i);
+                    i = 0;
                     expression = "";
                     for (String c: values)
                     {
@@ -50,70 +90,10 @@ public class StringCalculator extends Calculator
                 }
             }
             
-
-            /*else if (values.get(i) == "/")
-            {
-            values.set(i, "" + (Integer.parseInt(values.get(i+1)) / Integer.parseInt(values.get(i-1))));
-            values.remove(i-1);
-            values.remove(i);
-            i = i-2;
-
-            }
-            }
-             */
-            System.out.println(values.get(0));
-            
         }
-        //System.out.println(values.get(0));
-
-        while (s1.containsAdd() == true || s1.containsSubtract() == true)
-        {
-            for (int i = 0; i < values.size(); i++)
-            {
-                if (values.get(i) == "+")
-                {
-                    values.set(i, "" + (Integer.parseInt(values.get(i+1)) + Integer.parseInt(values.get(i-1))));
-                    values.remove(i-1);
-                    values.remove(i);
-                    i = i-2;
-                }
-                else if (values.get(i) == "-")
-                {
-                    values.set(i, "" + (Integer.parseInt(values.get(i+1)) - Integer.parseInt(values.get(i-1))));
-                    values.remove(i-1);
-                    values.remove(i);
-                    i = i-2;
-
-                }
-            }
-
-        }
-        //String value = "";
-        /*for (int i = 0; i < values.size(); i++)
-        {
-        value+=values.get(i);
-        }
-        /*ArrayList<String> values = new ArrayList<String>();
-        for (int i =0; i<expression.length(); i++)
-        {
-        values.add(expression.substring(i, i+1));
-        }
-        for (int i =0; i < values.size(); i+=)
-        {
-        if (values.get(i) == "*")
-        {
-        values.set(i, "" + values.get(i-1) * values.get(i+1);
-        i = i-3;
-        }
-        else if (values.get(i) == ("/")
-        {
-
-        }
-
-        }
-         */
-
-        //System.out.println(value);
+        System.out.println(values.get(0));
+        
+      
     }
 
 }
