105f71f04ee3fa7353537239d223c9e6aeee153a
aaggarw8 <aaggarw8@students.d125.org>
2016-05-24 09:21:55 -0500

    Add files via upload
    Now decimals work, and so does returning the number itself if there are no additional operations. Only thing left is to solve for 2-3 digit numbers.

--- StringCalc.java
+++ StringCalc.java
@@ -6,21 +6,48 @@ public class StringCalc
     public StringCalc(String ex1)
     {
         expression = ex1;
-        
+
     }
-    
+
     public String operations()
     {
-        
 
         StringAnalyzer s1 = new StringAnalyzer(expression);
         ArrayList<String> values = new ArrayList<String>();
 
         for (int i =0; i<expression.length(); i++)
         {
+
             values.add(expression.substring(i, i+1));
         }
 
+        for (int i = 0; i < values.size(); i++)
+        {
+            if (values.get(i).equals("."))
+            {
+                values.set(i, "" + values.get(i-1) + "." + values.get(i+1));
+                values.remove(i-1);
+                values.remove(i);
+                i=i-2;
+
+            }
+        }
+        if (!(s1.containsMultiply() || s1.containsDivide() || s1.containsAdd() || s1.containsSubtract()))
+        {
+            return expression;
+        }
+        int x = 1;
+        String expression2 = "";
+        int ctr = 0;
+        while (!(values.get(x-1).equals( "*")) && x<values.size())//, "/", "+", "-")
+        {
+            expression2+=values.get(x-1);
+            x++;
+            ctr++;
+        }
+        values.set(x-ctr, expression2);
+
+        
         while(s1.containsMultiply() || s1.containsDivide())
         {
             for (int i = 0; i < values.size(); i++)
@@ -56,9 +83,7 @@ public class StringCalc
                 }
             }
 
-            
             //System.out.println(values.get(0));
-
         }
         //System.out.println(values.get(0));
 
@@ -95,8 +120,9 @@ public class StringCalc
 
                 }
             }
-            
+
         }
+
         return(values.get(0));
     }
 }
