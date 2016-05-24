8ebc6b18ea9a8bcc4be12293920fc5d7efbc0774
aaggarw8 <aaggarw8@students.d125.org>
2016-05-24 09:20:44 -0500

    Add files via upload
    Decimals work now, and if there are no operations, then it just returns the number that was put in. Now all that is left to make sure 2 digit and 3 digit numbers work.

--- APCS Calculator/APCS Calculator/StringCalc.java
+++ APCS Calculator/APCS Calculator/StringCalc.java
@@ -2,23 +2,52 @@ import java.util.*;
 
 public class StringCalc
 { 
-    public StringCalc()
+    String expression;
+    public StringCalc(String ex1)
     {
-        
+        expression = ex1;
+
     }
-    
-    public String operations(String ex1)
+
+    public String operations()
     {
-        String expression = ex1;
 
         StringAnalyzer s1 = new StringAnalyzer(expression);
         ArrayList<String> values = new ArrayList<String>();
 
         for (int i =0; i<expression.length(); i++)
         {
+
             values.add(expression.substring(i, i+1));
         }
 
+        for (int i = 0; i < values.size(); i++)
+        {
+            if (values.get(i).equals("."))
+            {
+                values.set(i, "" + values.get(i-1) + "." + values.get(i+1));
+                values.remove(i-1);
+                values.remove(i);
+                i=i-2;
+
+            }
+        }
+        if (!(s1.containsMultiply() || s1.containsDivide() || s1.containsAdd() || s1.containsSubtract()))
+        {
+            return expression;
+        }
+        int x = 1;
+        String expression2 = "";
+        int ctr = 0;
+        while (!(values.get(x-1).equals( "*")) && x<values.size())//, "/", "+", "-")
+        {
+            expression2+=values.get(x-1);
+            x++;
+            ctr++;
+        }
+        values.set(x-ctr, expression2);
+
+        
         while(s1.containsMultiply() || s1.containsDivide())
         {
             for (int i = 0; i < values.size(); i++)
@@ -54,9 +83,7 @@ public class StringCalc
                 }
             }
 
-            
             //System.out.println(values.get(0));
-
         }
         //System.out.println(values.get(0));
 
@@ -93,8 +120,9 @@ public class StringCalc
 
                 }
             }
-            
+
         }
+
         return(values.get(0));
     }
 }
