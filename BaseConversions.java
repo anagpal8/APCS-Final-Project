deba2295baec2f3664d7b1708d959018dcc31792
anagpal8 <anagpal8@students.d125.org>
2016-05-26 21:39:25 -0500

    Update BaseConversions.java

--- BaseConversions.java
+++ BaseConversions.java
@@ -9,14 +9,18 @@ public class BaseConversions
 
     /**
      * @param number from the answerOutput of the 
-     * @return void
+     * @return a binary String
      * method sends the output to another class to calculate binary
      */
     public String conBaseThru(int num)
     {
         return convertBaseTwo(num);
     }
-    
+    /**
+     * @param number from the conBaseThru
+     * @return is recursive 
+     * converts the number to binary 
+     */
     public String convertBaseTwo(int number)
     {
 
@@ -29,7 +33,11 @@ public class BaseConversions
             return convertBaseTwo(number / 2) + Integer.toString(number % 2);
         }            
     }
-    
+    /**
+     * @param number from the answerOutput of the JFrame
+     * @return hex version of number
+     *  converts num to hexadecimal
+     */
     public String convertHex(int number)
     {
         String str = Integer.toHexString(number);
